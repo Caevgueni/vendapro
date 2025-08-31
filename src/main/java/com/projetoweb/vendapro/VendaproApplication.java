99fc06c55e60f88933e6c31b0ddbd8db1b34845d
Carlos Fernandes <itais1@hotmail.com>
2025-08-31 23:50:09 +0100

    project created

---
+++ src/main/java/com/projetoweb/vendapro/VendaproApplication.java
@@ -0,0 +1,13 @@
+package com.projetoweb.vendapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoweb/vendapro/VendaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoweb.vendapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
