408042a940c69fa16742f630be99d0043938868f
Carlos Fernandes <itais1@hotmail.com>
2025-09-05 00:26:31 +0100

    JPA repository, dependency injection, database seeding

--- src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
+++ src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
@@ -23,9 +23,10 @@ public class Testeconfig implements CommandLineRunner {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
-            
+		User u3 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");     
+		User u4 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		userrepository.saveAll(Arrays.asList(u1,u2));
+		userrepository.saveAll(Arrays.asList(u1,u2,u3,u4));
 		
 	}
 
--- src/main/java/com/projetoweb/vendapro/resurces/UserResurce.java
+++ src/main/java/com/projetoweb/vendapro/resurces/UserResurce.java
@@ -1,28 +1,37 @@
 package com.projetoweb.vendapro.resurces;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoweb.vendapro.entities.User;
+import com.projetoweb.vendapro.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResurce {
-	
+
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+
+	}
 	
-		User u= new User(2L, "carlos", "itais1@gmail.com", "99999999", "gfdudy");
-		
-		return ResponseEntity.ok(u);
-		
-		
-		
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
+	
 
 }
