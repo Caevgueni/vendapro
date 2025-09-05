00b78569d323fb5a22df0746d443338199473e93
Carlos Fernandes <itais1@hotmail.com>
2025-09-05 12:07:46 +0100

    order, Instant, ISO 8601 - PART -1

--- src/main/java/com/projetoweb/vendapro/entities/User.java
+++ src/main/java/com/projetoweb/vendapro/entities/User.java
@@ -1,6 +1,8 @@
 package com.projetoweb.vendapro.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
@@ -22,6 +24,10 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	
+	private List<Order> orders= new ArrayList<>();
+	
+	
 	public User() {
 
 	}
@@ -74,6 +80,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -92,4 +103,5 @@ public class User implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
+	
 }
