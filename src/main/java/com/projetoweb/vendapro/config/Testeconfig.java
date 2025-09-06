5c6c0005b65ed8c62e358334f5a6365fe905e251
Carlos Fernandes <itais1@hotmail.com>
2025-09-06 23:30:27 +0100

    OrderStatus enum

--- src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
+++ src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetoweb.vendapro.entities.Order;
 import com.projetoweb.vendapro.entities.User;
+import com.projetoweb.vendapro.entities.enums.OrderStatus;
 import com.projetoweb.vendapro.repositories.OrderRepository;
 import com.projetoweb.vendapro.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class Testeconfig implements CommandLineRunner {
 		User u1 = new User(null, "Yevgueni Fernandes", "yevgueni@gmail.com", "977777777", "123456");     
 		User u2 = new User(null, "Isnaba Bucal", "bucal@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.CANCELED, u1);
 		
 		userrepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
