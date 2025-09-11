a6e4d8a48f8194c214c1aff1c12fa3d9134520ab
Carlos Fernandes <itais1@hotmail.com>
2025-09-11 01:43:58 +0100

    Payment one-to-one association

--- src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
+++ src/main/java/com/projetoweb/vendapro/config/Testeconfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetoweb.vendapro.entities.Category;
 import com.projetoweb.vendapro.entities.Order;
 import com.projetoweb.vendapro.entities.OrderItem;
+import com.projetoweb.vendapro.entities.Payment;
 import com.projetoweb.vendapro.entities.Product;
 import com.projetoweb.vendapro.entities.User;
 import com.projetoweb.vendapro.entities.enums.OrderStatus;
@@ -85,7 +86,9 @@ public class Testeconfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
