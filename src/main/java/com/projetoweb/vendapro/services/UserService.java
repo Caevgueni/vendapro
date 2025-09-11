26e7c6710524609ccf124aee798d8b8516359f65
Carlos Fernandes <itais1@hotmail.com>
2025-09-11 22:59:24 +0100

    User delete

--- src/main/java/com/projetoweb/vendapro/services/UserService.java
+++ src/main/java/com/projetoweb/vendapro/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.projetoweb.vendapro.entities.User;
@@ -32,6 +33,10 @@ public class UserService {
 	public User  insert(User obj) {
 		return repository.save(obj);
 		
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 
 }
